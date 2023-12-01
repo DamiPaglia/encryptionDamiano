fae0fdaf96c1ee2c84ead3e2f1af79ec69082130
DamiPaglia <damianopagliarini360@gmail.com>
2023-12-01 14:36:36 +0000

    prima versione

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+  Vigenere v = new Vigenere();
+    new Thread (v).start();
+    t= new Thread (v);
+    t.start();
+  }
+  Vigenere v1= new Vigenere( 12,25,13,25,0);
+}
